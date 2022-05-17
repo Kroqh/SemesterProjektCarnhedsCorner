b3f3c5790014c72b13c12ae523d4cd747e8af0cd
Marcus Krogh <37517255+Kroqh@users.noreply.github.com>
2022-05-17 13:28:02 +0200

    S

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 
 public class Menu extends Product {
 
-	private ArrayList<Product> menuItem = new ArrayList<Product>();
+	private ArrayList<Product> menuItems = new ArrayList<Product>();
 	public Menu(float price, String name, String type, int id) {
 		super(price, name, type, id);
 	}
 	
 	public void addItemToMenu(Product product) {
-		menuItem.add(product);
+		menuItems.add(product);
 	}
 
 }
